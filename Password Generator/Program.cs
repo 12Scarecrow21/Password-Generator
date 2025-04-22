0994f62ceb33ce9f7c0a367f7b3dc606b6640b5d
ScareCrow <12scarecrow21@gmail.com>
2025-04-22 10:49:29 +0300

    Добавлено полное описание условий выбора для генерации паролей.

--- Password Generator/Program.cs
+++ Password Generator/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Welcome to Password Generator\nWhich password do you want to generate with");
+﻿Console.WriteLine("Welcome to Password Generator.\n" +
+    "Which password do you want to generate with:\n" +
+    "Select the number 1 if you want a password made up of only numbers\n" +
+    "select the number 2 if you want a password made up of only lowercase and uppercase letters\n" +
+    "select the number 3 if you want a password made up of a mixture of lowercase and uppercase letters and numbers.\n" +
+    "All passwords are generated up to 16 numbers.");
